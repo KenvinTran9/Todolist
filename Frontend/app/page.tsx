f23046a23198ebd6031f72b48e6406fa5ddf4b94
KenvinTran9 <tranquocnguyenkhang29@gmail.com>
2025-09-19 19:34:50 +0700

    refactor: simplify token management by utilizing AuthContext

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,47 +1,16 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useContext } from "react";
 import App from "@/components/App";
 import Header from "@/components/Header";
 import Login from "@/components/Login";
-
+import { AuthContext } from "./AuthProvider"; 
 export default function Home() {
-  const [token, setToken] = useState<string | null>(null);
-
-  useEffect(() => {
-    const savedToken = localStorage.getItem("token");
-    if (savedToken) {
-      fetch("http://localhost:4000/auth/verify", {
-        headers: { Authorization: `Bearer ${savedToken}` },
-      })
-        .then((res) => {
-          if (!res.ok) throw new Error("Invalid token");
-          setToken(savedToken);
-        })
-        .catch(() => {
-          localStorage.removeItem("token");
-          setToken(null);
-        });
-    }
-  }, []);
-
-  const handleLogin = (tk: string) => {
-    setToken(tk);
-    localStorage.setItem("token", tk);
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    setToken(null);
-  };
+  const { token, handleLogout, handleLogin } = useContext(AuthContext);
 
   return (
     <main className="main bg-white p-8 m-8 rounded-xl shadow-sm max-w-lg w-full">
-      <Header onLogout={handleLogout} token={token} />
-      {!token ? (
-        <Login onLogin={handleLogin} />
-      ) : (
-        <App token={token} />
-      )}
+      <Header token={token} onLogout={handleLogout} />
+      {!token ? <Login onLogin={handleLogin} /> : <App />}
     </main>
   );
 }
